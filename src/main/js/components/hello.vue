b802b9b7b6817fcf511dd00e2ec7a8f469fef08e
h1romas4 <h1romas4@gmail.com>
2019-09-28 05:29:58 +0900

    add vue example

--- src/main/js/components/hello.vue
+++ src/main/js/components/hello.vue
@@ -1,14 +1,23 @@
 <template>
     <div>
-        <p>{{ msg }}</p>
+        <p v-if="visble">{{ msg }}</p>
     </div>
 </template>
 
 <script>
 export default {
-    data () {
+    data: function() {
         return {
             msg: 'Hello, Vue component world!'
+        }
+    },
+    computed: {
+        visble: function() {
+            if(this.msg) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 }
