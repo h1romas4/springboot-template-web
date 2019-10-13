8813fe873e9c7e81fc7ab2e0a3056a8e0f47a46e
h1romas4 <h1romas4@gmail.com>
2019-10-14 00:58:03 +0900

    support signin security

--- src/main/resources/db/testdata/V01_99__Insert.sql
+++ src/main/resources/db/testdata/V01_99__Insert.sql
@@ -1,3 +1,4 @@
-INSERT INTO m_user (email, name, role, password) VALUES ('admin@example.com', 'アドミン', 'ADMIN', 'password');
-INSERT INTO m_user (email, name, role, password) VALUES ('user1@example.com', 'ユーザー1', 'USER', 'password');
-INSERT INTO m_user (email, name, role, password) VALUES ('user2@example.com', 'ユーザー2', 'USER', 'password');
+/* "password" bcrypted "$2a$08$PdNiM0gEeAOwPqo4LMuYWuorzRaaLifnKHQ.stQQ5buFihervJ6k2" */
+INSERT INTO m_user (email, name, role, password) VALUES ('admin@example.com', 'アドミン', 'ROLE_ADMIN', '$2a$08$PdNiM0gEeAOwPqo4LMuYWuorzRaaLifnKHQ.stQQ5buFihervJ6k2');
+INSERT INTO m_user (email, name, role, password) VALUES ('user1@example.com', 'ユーザー1', 'ROLE_USER', '$2a$08$PdNiM0gEeAOwPqo4LMuYWuorzRaaLifnKHQ.stQQ5buFihervJ6k2');
+INSERT INTO m_user (email, name, role, password) VALUES ('user2@example.com', 'ユーザー2', 'ROLE_USER', '$2a$08$PdNiM0gEeAOwPqo4LMuYWuorzRaaLifnKHQ.stQQ5buFihervJ6k2');
