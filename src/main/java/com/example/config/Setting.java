f2d7e2ba1d998f8dc2e377488e44c07dbe6af917
h1romas4 <h1romas4@gmail.com>
2019-10-18 03:14:28 +0900

    remove unused annotation on springboot 2.2

--- src/main/java/com/example/config/Setting.java
+++ src/main/java/com/example/config/Setting.java
@@ -3,14 +3,12 @@ package com.example.config;
 import java.io.Serializable;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 /**
  * Setting
  *
  * @see src/resources/application.yml
  */
-@Component
 @ConfigurationProperties(prefix = "setting")
 public class Setting implements Serializable {
     private static final long serialVersionUID = 1L;
