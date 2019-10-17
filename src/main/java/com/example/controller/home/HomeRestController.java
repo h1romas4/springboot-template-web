ff72b853cb7a59c6516c85b18d6371f2ab3b1420
h1romas4 <h1romas4@gmail.com>
2019-10-18 03:31:31 +0900

    modify List.of from ArrayList

--- src/main/java/com/example/controller/home/HomeRestController.java
+++ src/main/java/com/example/controller/home/HomeRestController.java
@@ -1,6 +1,5 @@
 package com.example.controller.home;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 class HomeRestController {
     @GetMapping("/vue")
     List<String> home(Model model) {
-        return new ArrayList<String>() {
-            { add("Hello,"); }
-            { add("Vue.js"); }
-            { add("world."); }
-        };
+        return List.of("Hello,", "Vue.js", "world.");
     }
 }
