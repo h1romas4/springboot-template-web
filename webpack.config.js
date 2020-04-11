dffe4b5a6912f67fe904ac1f751613f2c72a1a54
h1romas4 <h1romas4@gmail.com>
2020-04-11 17:42:47 +0900

    support css build

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,96 @@
 const webpack = require('webpack');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: 'production',
-    entry: {
-        vendor: ['vue', 'axios', 'jquery', 'popper.js', 'bootstrap'],
-        home: './src/main/js/home.js',
-        signin: './src/main/js/signin.js',
-        dashboard: './src/main/js/dashboard.js',
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: path.join(__dirname, '/src/main/resources/static/js'), // eslint-disable-line
-        publicPath: "/js/",
-    },
-    optimization: {
-        splitChunks: {
-            name: 'vendor',
-            chunks: 'initial'
-        }
-    },
-    plugins: [
-        // for bootstrap jQuery plugin
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-        }),
-        new VueLoaderPlugin()
+  mode: 'production',
+  entry: {
+    vendor: [
+      'vue',
+      'axios',
+      'jquery',
+      'popper.js',
+      'bootstrap',
+      './src/main/css/style.css'
     ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }]
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.js', '.vue'],
-        modules: [
-            "node_modules"
-        ],
-        alias: {
-            'vue$': 'vue/dist/vue.esm.js'
-        }
-    },
-    performance: {
-        maxAssetSize: 3000000,
-        maxEntrypointSize: 3000000
+    home: './src/main/js/home.js',
+    signin: './src/main/js/signin.js',
+    dashboard: './src/main/js/dashboard.js',
+  },
+  output: {
+    filename: 'js/[name].bundle.js',
+    path: path.join(__dirname, '/src/main/resources/static'), // eslint-disable-line
+    publicPath: "/js/",
+  },
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks: 'initial'
     }
+  },
+  plugins: [
+    // for bootstrap jQuery plugin
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    }),
+    new VueLoaderPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }]
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '/css',
+            },
+          },
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.(ttf|otf|eot|woff|woff2|svg)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: "[name].[ext]",
+            outputPath: 'fonts/'
+          }
+        }]
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+    modules: [
+      "node_modules"
+    ],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  performance: {
+    maxAssetSize: 3000000,
+    maxEntrypointSize: 3000000
+  }
 };
