d9296054ec74662f406913d653fffe4e3456714b
h1romas4 <h1romas4@gmail.com>
2020-04-13 01:46:06 +0900

    ripstick

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,30 +3,29 @@ const path = require('path');
 const common = require('./webpack.config.js');
 
 module.exports = merge(common, {
-    devtool: 'source-map',
-    devServer: {
-      inline: true,
-      contentBase: [
-          path.join(__dirname, '/src/main/resources/templates'), // eslint-disable-line
-      ],
-      before: function(app) {
-        app.use('/api', function (req, res) {
-          res.type('application/json')
-          res.sendFile(path.join(__dirname, '/src/test/js/json', req.originalUrl + ".json")) // eslint-disable-line
-        })
-      },
-      watchContentBase: true,
-      port: 9080,
-      open: true,
-      openPage: "home.html"
+  devtool: 'source-map',
+  devServer: {
+    inline: true,
+    contentBase: [
+        path.join(__dirname, '/src/main/resources/templates'), // eslint-disable-line
+    ],
+    before: function(app) {
+      app.use('/api', function (req, res) {
+        res.type('application/json')
+        res.sendFile(path.join(__dirname, '/src/test/js/json', req.originalUrl + ".json")) // eslint-disable-line
+      })
     },
-    resolve: {
-        alias: {
-          'vue$': 'vue/dist/vue.js'
-        }
-    },
-    performance: {
-      hints: false
-    }
+    watchContentBase: true,
+    port: 9080,
+    open: true,
+    openPage: "home.html"
+  },
+  resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.js'
+      }
+  },
+  performance: {
+    hints: false
   }
-);
+});
