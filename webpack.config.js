7caff9ab614892d1b07a4da7682be93febdf7a0d
h1romas4 <h1romas4@gmail.com>
2022-04-12 15:13:44 +0900

    add @babel/plugin-transform-runtime

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,8 @@ module.exports = {
         use: [{
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
           }
         }]
       },
