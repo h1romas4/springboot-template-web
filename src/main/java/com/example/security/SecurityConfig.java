1eaee3cca0102a81c6e36e19cba3cca6b3a9ec42
h1romas4 <h1romas4@gmail.com>
2023-08-22 17:26:36 +0900

    update spring security (WIP)

--- src/main/java/com/example/security/SecurityConfig.java
+++ src/main/java/com/example/security/SecurityConfig.java
@@ -2,20 +2,23 @@ package com.example.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
         // Force the use of HTTPS
         // https://devcenter.heroku.com/articles/preparing-a-spring-boot-app-for-production-on-heroku#force-the-use-of-https
         // http.requiresChannel()
@@ -23,10 +26,10 @@ public class SecurityConfig {
         //     .requiresSecure();
         // login authorize
         http.authorizeRequests((authz) -> authz
-            .requestMatchers("/css/**", "/image/**", "/js/**", "/fonts/**")
+            .requestMatchers(mvc.pattern("/css/**"), mvc.pattern("/image/**"), mvc.pattern("/js/**"), mvc.pattern("/fonts/**"))
             .permitAll());
         http.authorizeRequests((authz) -> authz
-            .requestMatchers("/admin/**").hasRole("ADMIN")
+            .requestMatchers(mvc.pattern("/admin/**")).hasRole("ADMIN")
             .anyRequest()
             .permitAll());
         http.formLogin((authz) -> authz
@@ -44,6 +47,12 @@ public class SecurityConfig {
             .permitAll());
         return http.build();
     }
+
+    @Scope("prototype")
+	@Bean
+	MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+		return new MvcRequestMatcher.Builder(introspector);
+	}
 
     @Bean
     public PasswordEncoder passwordEncoder() {
